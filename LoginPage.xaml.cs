1d123d004a4845a9398f50bed681125555c90fc1
Rda23 <darren.assale@ecole-isitech.fr>
2024-12-20 17:48:44 +0100

    feat: create login and subscribe page

---
+++ LoginPage.xaml.cs
@@ -0,0 +1,28 @@
+namespace PlanMyMeal_Domain;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+    private async void Btn_Main(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//Main");
+    }
+    private async void Btn_Subscribe(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new SubscribePage());
+    }
+    private void Btn_PageMode(object sender, EventArgs e)
+    {
+        if (Application.Current.UserAppTheme == AppTheme.Dark)
+        {
+            Application.Current.UserAppTheme = AppTheme.Light; //LightMode
+        }
+        else
+        {
+            Application.Current.UserAppTheme = AppTheme.Dark; // DarkMode
+        }
+    }
+}
---
+++ SubscribePage.xaml.cs
@@ -0,0 +1,23 @@
+namespace PlanMyMeal_Domain;
+
+public partial class SubscribePage : ContentPage
+{
+	public SubscribePage()
+	{
+		InitializeComponent();
+	}
+    private async void Btn_Main(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//Main");
+    }
+    private async void Btn_Next(object sender, EventArgs e)
+    {
+        Login.IsVisible = false;
+        Password.IsVisible = true;
+        await Task.Delay(500);
+    }
+    private async void Btn_Login(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//Login");
+    }
+}
