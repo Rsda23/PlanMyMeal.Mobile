d2ca2a2337baed083d4ecdc3ea4a81c3aa66edcc
Rda23 <darren.assale@ecole-isitech.fr>
2025-01-17 14:42:16 +0100

    feat: added dark mode toggle button and coordinates section to profile page

--- ProfilPage.xaml.cs
+++ ProfilPage.xaml.cs
@@ -10,6 +10,10 @@ public partial class ProfilPage : ContentPage
     {
         await Shell.Current.GoToAsync("//Setting");
     }
+    private async void Btn_Login(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//Login");
+    }
     public async void ChangeImage(object sender, EventArgs e)
     {
         var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
@@ -23,4 +27,29 @@ public partial class ProfilPage : ContentPage
             ProfilImage.Source = ImageSource.FromStream(() => flux);
         }
     }
+    private void OnDarkModeToggled(object sender, ToggledEventArgs e)
+    {
+        bool isDarkMode = e.Value;
+
+        Application.Current.UserAppTheme = isDarkMode ? AppTheme.Dark : AppTheme.Light;
+    }
+    //private void OnThemeChanged(object sender, EventArgs e)
+    //{
+    //    var picker = (Picker)sender;
+    //    var selectedTheme = picker.SelectedItem.ToString();
+
+    //    // Changer le thème selon le choix
+    //    switch (selectedTheme)
+    //    {
+    //        case "Clair":
+    //            Application.Current.UserAppTheme = AppTheme.Light;
+    //            break;
+    //        case "Sombre":
+    //            Application.Current.UserAppTheme = AppTheme.Dark;
+    //            break;
+    //        default:
+    //            Application.Current.UserAppTheme = AppTheme.Unspecified;
+    //            break;
+    //    }
+    //}
 }
