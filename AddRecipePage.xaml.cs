39553a6200a89913e0e9074c835bdf088b39f879
Rda23 <darren.assale@ecole-isitech.fr>
2025-01-17 17:59:59 +0100

    fix: added add button, search bar, and initial filter button

---
+++ AddRecipePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace PlanMyMeal_Domain;
+
+public partial class AddRecipePage : ContentPage
+{
+	public AddRecipePage()
+	{
+		InitializeComponent();
+	}
+}
--- RecipePage.xaml.cs
+++ RecipePage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Collections.ObjectModel;
+
 namespace PlanMyMeal_Domain;
 
 public partial class RecipePage : ContentPage
@@ -6,4 +8,14 @@ public partial class RecipePage : ContentPage
 	{
 		InitializeComponent();
 	}
+    private async void AddRecipe(object sender, TappedEventArgs e)
+	{
+		await Shell.Current.GoToAsync("//AddRecipe");
+	}
+
+	private void OnFilterClicked(Object sender, EventArgs e)
+	{
+        FilterPanel.IsVisible = !FilterPanel.IsVisible;
+
+    }
 }
