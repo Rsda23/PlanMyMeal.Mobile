54e6d76cfbe06a4784cf67cffbf9f52d5354338d
Rsda23 <rsda23.0@gmail.com>
2025-09-08 14:24:46 +0200

    refactor(login): migrate login page logic to mvvm

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -1,14 +1,106 @@
-﻿using PlanMyMeal_Domain.Services;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PlanMyMeal.Domain.Models;
+using PlanMyMeal_Domain.Services;
 
 namespace PlanMyMeal_Domain.ViewModels
 {
 
-    public partial class LoginViewModel
+    public partial class LoginViewModel : ViewModelBase
     {
         private readonly UsersService _usersService;
+
         public LoginViewModel(UsersService usersService)
         {
             _usersService = usersService;
+        }
+
+        [ObservableProperty]
+        private string email = string.Empty;
+
+        [ObservableProperty]
+        private string password = string.Empty;
+
+        [ObservableProperty]
+        private string error = string.Empty;
+        [ObservableProperty]
+        private bool isError = false;
+
+        [ObservableProperty]
+        private bool isVisible = true;
+
+
+        [RelayCommand]
+        private async Task NavigateTo(string destination)
+        {
+            if (string.IsNullOrEmpty(destination))
+            {
+                throw new Exception("La destination est vide ou null");
+            }
+
+            if (destination == "home")
+            {
+                await Shell.Current.GoToAsync("//Main");
+            }
+            else if (destination == "subscribe")
+            {
+                await Shell.Current.GoToAsync("//Subscribe");
+            }
+            else if (destination == "forgout")
+            {
+                await Shell.Current.GoToAsync("//Forgout");
+            }
+        }
+
+        [RelayCommand]
+        private async Task ValidLogin()
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(Email) || string.IsNullOrWhiteSpace(Password))
+                {
+                    throw new Exception("Tous les champs doivent être remplis.");
+                }
+
+                User? user = await _usersService.GetUserByEmail(Email);
+
+                if (user == null)
+                {
+                    Error = "Email ou mot de passe incorrect.";
+                    IsError  = true;
+                    return;
+                }
+
+                bool password = PasswordHashing.ConfirmPassword(Password, user.HashedPassword);
+
+                if (!password)
+                {
+                    Error = "Email ou mot de passe incorrect.";
+                    IsError = true;
+                    return;
+                }
+
+                await NavigateTo("home");
+            }
+            catch (Exception ex)
+            {
+                Error = ex.Message;
+                IsError = true;
+            }
+        }
+
+        [RelayCommand]
+        private void PasswordVisibility()
+        {
+            IsVisible = !IsVisible;
+        }
+
+
+        public void ResetVisibility()
+        {
+            IsError = false;
+            Password = string.Empty;
+            Email = string.Empty;
         }
     }
 }
--- Views/Login/LoginPage.xaml.cs
+++ Views/Login/LoginPage.xaml.cs
@@ -1,11 +1,9 @@
-using MongoDB.Driver;
 using PlanMyMeal_Domain.ViewModels;
 
 namespace PlanMyMeal_Domain;
 
 public partial class LoginPage : ContentPage
 {
-    private bool _isPasswordVisible = false;
     private readonly LoginViewModel _loginViewModel;
     public LoginPage(LoginViewModel model)
 	{
@@ -16,83 +14,6 @@ public partial class LoginPage : ContentPage
     protected override void OnAppearing()
     {
         base.OnAppearing();
-
-        EmailEntry.Text = string.Empty;
-        PasswordEntry.Text = string.Empty;
-
-        ErrorLogin.IsVisible = false;
-
-    }
-    private async void Btn_Main(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync("//Main");
-    }
-    private async void Go_Main()
-    {
-        await Shell.Current.GoToAsync("//Main");
-    }
-    private async void Btn_Subscribe(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync("//Subscribe");
-    }
-
-    private async void Btn_Forgout(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new ForgoutPage());
-    }
-    private void ValidLogin(object sender, EventArgs e)
-    {
-        try
-        {
-            string inputEmail = EmailEntry.Text;
-            string inputPassword = PasswordEntry.Text;
-            string emailConnected = inputEmail;
-
-            var collection = _mongoDbService.GetCollection<User>("users");
-            var user = collection.Find(user => user.Email == inputEmail).FirstOrDefault();
-
-            if (string.IsNullOrWhiteSpace(inputEmail) || string.IsNullOrWhiteSpace(inputPassword))
-            {
-                throw new Exception("Tous les champs doivent être remplis.");
-            }
-
-            if (user == null)
-            {
-                ErrorLogin.Text = "Email ou mot de passe incorrect.";
-                ErrorLogin.IsVisible = true;
-                return;
-            }
-
-            bool password = PasswordHashing.ConfirmPassword(inputPassword, user.HashedPassword);
-
-            if (!password)
-            {
-                ErrorLogin.Text = "Email ou mot de passe incorrect.";
-                ErrorLogin.IsVisible = true;
-                return;
-            }
-
-            Go_Main();
-        }
-        catch (Exception ex)
-        {
-            ErrorLogin.Text = ex.Message;
-            ErrorLogin.IsVisible = true;
-        }
-    }
-    private void PasswordVisibility(object sender, EventArgs e)
-    {
-        _isPasswordVisible = !_isPasswordVisible;
-        PasswordEntry.IsPassword = !_isPasswordVisible;
-        if (_isPasswordVisible)
-        {
-            EyeYes.IsVisible = false;
-            EyeNo.IsVisible = true;
-        }
-        else
-        {
-            EyeYes.IsVisible = true;
-            EyeNo.IsVisible = false;
-        }
+        _loginViewModel.ResetVisibility();
     }
 }
