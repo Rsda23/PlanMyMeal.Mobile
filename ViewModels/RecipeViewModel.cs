f806fef8572ec5a56c4b3052b29551e4fa51d3b4
Rsda23 <rsda23.0@gmail.com>
2025-09-09 14:35:45 +0200

    refactor(recipe): migrate recipe page logic to mvvm

---
+++ ViewModels/RecipeViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using PlanMyMeal_Domain.Interfaces;
+
+namespace PlanMyMeal_Domain.ViewModels
+{
+    public partial class RecipeViewModel
+    {
+        private readonly IRecipesService _recipesService;
+        public RecipeViewModel(IRecipesService recipesService)
+        {
+            _recipesService = recipesService;
+        }
+
+
+        [RelayCommand]
+        private async Task Filter()
+        {
+
+        }
+
+
+        private async Task DisplayRecipe()
+        {
+           
+        }
+    }
+}
--- Views/Recipe/RecipePage.xaml.cs
+++ Views/Recipe/RecipePage.xaml.cs
@@ -1,42 +1,15 @@
-using CommunityToolkit.Maui.Views;
-using MongoDB.Driver;
-using PlanMyMeal.Infrastructure;
+using PlanMyMeal_Domain.ViewModels;
 
 namespace PlanMyMeal_Domain;
 
 public partial class RecipePage : ContentPage
 {
-    private readonly MongoDbService _mongoService;
-    public RecipePage(MongoDbService mongoService)
+    private readonly RecipeViewModel _recipeViewModel;
+    public RecipePage(RecipeViewModel model)
 	{
         InitializeComponent();
 
-        _mongoService = mongoService;
-        LoadData();
-    }
-    private void LoadData()
-    {
-        var collection = _mongoService.GetCollection<User>("users");
-        var data = collection.Find(_ => true).ToList();
-
-        Console.WriteLine($"Nombre d'éléments récupérés : {data.Count}");
-        RecipeListView.ItemsSource = data;
-    }
-    private async void AddRecipe(object sender, EventArgs e)
-	{
-		await Shell.Current.GoToAsync("//AddRecipe");
-	}
-    private async void FilterButton(object sender, EventArgs e)
-    {
-        var result = await this.ShowPopupAsync(new Filter());
-
-        if (result != null)
-        {
-            ApplyFilter(result.ToString() ?? "Default");
-        }
-    }
-    private void ApplyFilter(string category)
-    {
-        Console.WriteLine("Filtrage...");
+        _recipeViewModel = model;
+        BindingContext = _recipeViewModel;
     }
 }
