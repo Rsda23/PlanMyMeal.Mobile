66a5d03baae972e4096fde480b4623bf6b9ddbe8
Rsda23 <rsda23.0@gmail.com>
2025-09-24 12:23:54 +0200

    refactor(recipe): rename recipe detail to recipe detail page

--- Views/Recipe/RecipeDetail.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace PlanMyMeal_Domain.Views.Recipe;
-
-public partial class RecipeDetail : ContentPage
-{
-	public RecipeDetail()
-	{
-		InitializeComponent();
-	}
-}
---
+++ Views/Recipe/RecipeDetailPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace PlanMyMeal_Domain.Views.Recipe;
+
+public partial class RecipeDetailPage : ContentPage
+{
+	public RecipeDetailPage()
+	{
+		InitializeComponent();
+	}
+}
